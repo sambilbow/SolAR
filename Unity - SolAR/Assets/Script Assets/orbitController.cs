7e4dda27f0de0a6afe59c6e318adf99fb5cf1245
Samuel Bilbow <samuel.bilbow@icloud.com>
2018-11-19 12:38:20 +0000

    SolAR Unity Project
    Working 3D spatialisation. Using Helm synth at runtime.

---
+++ Unity - SolAR/Assets/Script Assets/orbitController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbitController : MonoBehaviour {
+
+	public float rotateSpeed = 1.0f;
+	public Vector3 spinAxis  = new Vector3(0,1,0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	this.transform.RotateAround(Vector3.zero,spinAxis,rotateSpeed);	
+
+	}
+
+	void rotate () {
+
+	
+
+	}
+}
---
+++ Unity - SolAR/Assets/noteOn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noteOn : MonoBehaviour {
+
+	public AudioHelm.HelmController helmController;
+	public int midiNote;
+	public float velocity;
+	
+
+	// Use this for initialization
+	void Start () {
+	startNote();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void startNote (){
+
+		helmController.NoteOn(midiNote, velocity, 5.0f);
+
+	}
+}
