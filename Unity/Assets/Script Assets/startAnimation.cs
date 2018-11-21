2157235a7a3d5b2ca034cb020f1f7e9936cb509e
Samuel Bilbow <samuel.bilbow@icloud.com>
2018-11-21 13:04:06 +0000

    GUI changes
    loadscreen final
    main menu final
    about app added

---
+++ Unity/Assets/Script Assets/startAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startAnimation : MonoBehaviour {
+
+	float t;
+    Vector2 startPosition = new Vector2(-540,960);
+    Vector2 endPosition = new Vector2(540,960);
+    float timeToReachTarget = 1f;
+
+	// Use this for initialization
+    void Start () 
+    {
+    endPosition = transform.position;	
+    }
+	
+	
+    
+    
+    // Update is called once per frame
+	void Update () 
+    {	
+    t += Time.deltaTime/timeToReachTarget;
+    transform.position = Vector2.Lerp(startPosition, endPosition, t);
+    }
+     
+     
+    
+    
+    
+    public void SetDestination(Vector2 destination, float time)
+    {
+        t = 0;
+        startPosition = transform.position;
+        timeToReachTarget = time;
+        endPosition = destination; 
+    } 
+	
+
+
+    
+}
