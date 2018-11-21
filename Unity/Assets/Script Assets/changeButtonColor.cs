94cff3a164569f40cdf824ebda47534b1469a0b4
Samuel Bilbow <samuel.bilbow@icloud.com>
2018-11-21 19:29:13 +0000

    UI consolidation. Mute buttons UI now toggling in solarSystem.

---
+++ Unity/Assets/Script Assets/changeButtonColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class changeButtonColor : MonoBehaviour {
+
+	public Button button;
+
+	public void buttonColorChanger (byte r, byte g, byte b)
+		{
+		button.GetComponent<Image>().color = Color.yellow;
+		}
+	
+}
+
+
+
