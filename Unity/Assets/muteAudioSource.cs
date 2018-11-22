8444706c3bd41350512b92256a861cfbbb0f1b0b
Samuel Bilbow <samuel.bilbow@icloud.com>
2018-11-22 09:47:41 +0000

    Tidied up scenes. Mute buttons working with Audio

---
+++ Unity/Assets/muteAudioSource.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class muteAudioSource : MonoBehaviour {
+
+	// Use this for initialization
+	public void muter () 
+	{
+        var audioSource = GetComponent<AudioSource>();
+        audioSource.mute = !audioSource.mute;
+    }
+}
---
+++ Unity/Assets/muteToggler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+ 
+ [RequireComponent(typeof(AudioSource))]
+ public class muteToggler : MonoBehaviour
+ {
+     public void ToggleMute()
+     {
+         var a = GetComponent<AudioSource>();
+         a.mute = !a.mute;
+     }
+ }
