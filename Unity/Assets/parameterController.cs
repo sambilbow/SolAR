77e8c78249389bb255a3e8e24c7660cee4a75574
Samuel Bilbow <samuel.bilbow@icloud.com>
2018-11-25 19:10:11 +0000

    Started work on planet property parameter control

---
+++ Unity/Assets/parameterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioHelm;
+
+public class parameterController : MonoBehaviour {
+
+	
+	
+	// Use this for initialization
+	void Start () {
+
+		// Lets find all of the 
+
+		// Find object diameter from rectTransform scale.	
+		RectTransform objectRectTransform = this.gameObject.GetComponent<RectTransform>();
+		float objectDiameter = objectRectTransform.localScale.x;
+
+		// Find object orbital and rotational frequencies from orbital controller script.
+		var orbitControllerScript = this.gameObject.GetComponent<orbitController>();
+		float objectOrbitalFrequency = orbitControllerScript.orbitSpeed;
+		float objectRotationalFrequency = orbitControllerScript.rotateSpeed;
+
+		
+		// Setting parameters
+		HelmController controller = this.gameObject.GetComponent<AudioHelm.HelmController>();
+		
+		controller.SetParameterAtIndex(1,);
+		controller.SetParameterAtIndex(2,);
+		controller.SetParameterAtIndex(3,);
+
+	}
+	
+	
+}
